ed790fed79d91ab1016c998c34f8f15db6653ece
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-25 17:44:44 -0400

    fix style

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -2,12 +2,14 @@
 """
 Module with function that transposes
 """
+
+
 def matrix_transpose(matrix):
     """Function that transposes"""
     new_matrix = []
     for i in range(len(matrix[0])):
         row = []
         for j in range(len(matrix)):
-                row.append(matrix[j][i])
+            row.append(matrix[j][i])
         new_matrix.append(row)
     return new_matrix
