dd0d75f398651ae2075db2921469a0940365f65d
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-25 15:10:27 -0400

    lets see

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """Function that calculates the shape of a matrix"""
-from numpy import shape, array
 def matrix_shape(matrix):
-    shapeOfmatrix = array(matrix)
-    shapeOfmatrix = shape(shapeOfmatrix)
-    return list(shapeOfmatrix)
+    shape = []
+    shape.append(len(matrix))
+    for i in range(0, shape[0]):
+        if i == shape[0] - 1:
+            shape.append(len(matrix[i]))
+            if type(matrix[i][0]) is list:
+                shape.append(len(matrix[i][0]))
+    return shape
+"""
+[
+    [
+         [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]
+                                                                    ],
+        [
+            [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]
+                                                                             ]
+                                                                                ]
+"""
