ebc306f3760437daaf14a9381e7b4c389969c9a7
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-26 16:48:00 -0400

    Fix condition

--- math/linear_algebra/7-main.py
+++ math/linear_algebra/7-main.py
@@ -14,3 +14,16 @@ mat1[1].append(5)
 print(mat1)
 print(mat4)
 print(mat5)
+print("My tests")
+m1 = [[4, -7, 56, 2], [5, 106, 7, 2]]
+m2 = [[2, -6, 3], [0, -6, 3]]
+m = cat_matrices2D(m1, m2)
+print(m)
+m1 = [[484, 247], [554, 16], [5, 88]]
+m2 = [[233, -644, 325], [406, -16, 33], [765, 34, -39]]
+m = cat_matrices2D(m1, m2, axis=0)
+print(m)
+m1 = [[-54, -87, 95], [54, 16, -72]]
+m2 = [[12, 63, 79], [-10, 69, -9], [76, 45, -11]]
+m = cat_matrices2D(m1, m2, axis=1)
+print(m)
