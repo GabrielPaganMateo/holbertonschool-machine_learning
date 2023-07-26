6fab2e3a12c2eb7df522a2c32e4730a85710291e
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-26 10:41:27 -0400

    Edge case by axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -14,12 +14,16 @@ def cat_matrices2D(mat1, mat2, axis=0):
     for i in range(len(mat2)):
         matrix2.append(mat2[i].copy())
     if axis == 0:
+        if len(matrix1[0]) != len(matrix1[0]):
+            return None
         for arr in matrix1:
             new_matrix.append(arr[:])
         for arr in matrix2:
             new_matrix.append(arr[:])
         return new_matrix
     elif axis == 1:
+        if len(matrix1) != len(matrix2):
+            return None
         for i in range(len(matrix1)):
             matrix1[i].extend(matrix2[i])
         new_matrix = matrix1[:]
