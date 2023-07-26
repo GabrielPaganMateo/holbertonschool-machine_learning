223c2ac8907ace1417bcab6ff83c5de4cfd3b5ae
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-26 10:14:24 -0400

    Task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module with function
+"""
+
+"""*** axis 0 means x axis and axis 1 means y axis ????? 
+"""
+def cat_matrices2D(mat1, mat2, axis=0):
+    new_matrix = []
+    matrix1 = []
+    matrix2 = []
+    for i in range(len(mat1)):
+        matrix1.append(mat1[i].copy())
+    for i in range(len(mat2)):
+        matrix2.append(mat2[i].copy())
+    if axis == 0:
+        for arr in matrix1:
+            new_matrix.append(arr[:])
+        for arr in matrix2:
+            new_matrix.append(arr[:])
+        return new_matrix
+    elif axis == 1:
+        for i in range(len(matrix1)):
+            matrix1[i].extend(matrix2[i])
+        new_matrix = matrix1[:]
+        return new_matrix
