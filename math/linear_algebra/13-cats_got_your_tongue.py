30aecca07ec895ae7bc2d08e770929626c19c85d
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-27 13:23:58 -0400

    Task 13

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to concatenate matrixes
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    new_matrix = np.concatenate(mat1, mat2, axis)
+    return new_matrix
