53ab57a983b2f253d05cf173e33bf471fdbe0ff2
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-27 14:01:46 -0400

    blank line

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -4,6 +4,7 @@ Functon that performs matrix multiplication
 """
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
     """Functoin tht performs matrix multiplication"""
     new_matrix = np.matmul(mat1, mat2)
