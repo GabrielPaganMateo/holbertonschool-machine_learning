a230667db5d3951cf8031a693415fe0daa6c0c49
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-27 08:17:13 -0400

    Task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module with function that calculates shape of np.array
+"""
+
+
+def np_shape(matrix):
+    """Function that calculates shape of array"""
+    shape = matrix.shape
+    return shape
