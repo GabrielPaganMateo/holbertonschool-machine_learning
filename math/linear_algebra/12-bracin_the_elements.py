7bee8be38cb62e42c3afc3ce7579f4fcb7ff7d6b
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-27 11:27:44 -0400

    Element operatons

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,14 @@
+#!/bin/usr/env python3
+"""
+Module with function that returns matrix operations
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """Perform matrix operations"""
+    sum = mat1 + mat2
+    difference = mat1 - mat2
+    print(type(mat2))
+    product = mat1 * mat2
+    quotient = mat1 / mat2
+    return sum, difference, product, quotient
