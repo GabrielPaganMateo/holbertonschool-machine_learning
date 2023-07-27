9c9f6cb52e08bd32535b31c5ecc58187e6017015
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-27 13:55:29 -0400

    Task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Functon that performs matrix multiplication
+"""
+
+
+def np_matmul(mat1, mat2):
+    """Functoin tht performs matrix multiplication"""
+    new_matrix = mat1 @ mat2
+    return new_matrix
