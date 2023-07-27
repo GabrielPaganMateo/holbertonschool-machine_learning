dfca17f3a0eb693d329acbc87fe505cefdf54137
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-27 09:39:11 -0400

    Transposing matrixes

---
+++ math/linear_algebra/11-the_western_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module with function that can transpose
+"""
+
+
+def np_transpose(matrix):
+    """function that transposes matrixes"""
+    trasnposed = matrix.T
+    return trasnposed
