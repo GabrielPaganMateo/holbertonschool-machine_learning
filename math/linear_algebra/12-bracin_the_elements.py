afb6845847fbb082ca4ed6820a1f060fd8a31479
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-27 11:31:13 -0400

    Remove print

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -8,7 +8,6 @@ def np_elementwise(mat1, mat2):
     """Perform matrix operations"""
     sum = mat1 + mat2
     difference = mat1 - mat2
-    print(type(mat2))
     product = mat1 * mat2
     quotient = mat1 / mat2
     return sum, difference, product, quotient
