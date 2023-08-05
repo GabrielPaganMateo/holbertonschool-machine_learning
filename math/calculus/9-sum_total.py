3aabaca6c6bbe01b0fe1aba226b5065c167e55b6
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-08-05 16:32:50 -0400

    fix she bang

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3 env
+#!/usr/bin/env python3
 """
 Module with def summation function
 """
 
 
 def summation_i_squared(n):
+    """Summation function"""
     sum = 0
     for i in range(1, n + 1):
         sum += i**2
