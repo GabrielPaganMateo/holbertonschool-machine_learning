33b4a4ce71386b471f42baf5d7fe69ab6962475c
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-08-05 17:02:12 -0400

    function

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,12 +5,8 @@ Module with def summation function
 
 
 def summation_i_squared(n):
-    # base case: if n is not a valid number, return None
-    if not isinstance(n, int) or n < 1:
-        return None
-    # base case: the sum of the first 1 natural number's squares is 1
-    elif n == 1:
-        return 1
+    """Summation function"""
+    if isinstance(n, int) and n >= 1:
+        return n * (n + 1) * (2 * n + 1) // 6
     else:
-        # recursive case: the sum is n^2 plus the sum of the squares of the first n-1 natural numbers
-        return n**2 + summation_i_squared(n-1)
+        return None
